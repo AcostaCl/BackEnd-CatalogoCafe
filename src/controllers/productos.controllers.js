2920c1b2ce83e1a3a80084ec0cd6e6b4d745901d
Celina <idkmahee@gmail.com>
2025-08-20 21:14:13 -0300

    separe la logica de la funcion de prueba y se agregó al controlador

---
+++ src/controllers/productos.controllers.js
@@ -0,0 +1,9 @@
+export const test = (req, res) => {
+  res.status(200);
+  res.send("Primera prueba desde el backend");
+};
+
+//agregar funcion para crear producto
+
+//agregar funcion para editar producto
+  

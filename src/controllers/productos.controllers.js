6292926fb4dbc376522319e52a50c41205f11459
Celina <idkmahee@gmail.com>
2025-08-30 20:28:58 -0300

    quite el console.log de prueba de crear producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,3 +1,5 @@
+import Producto from "../models/producto.js";
+
 export const test = (req, res) => {
   res.status(200);
   res.send("Primera prueba desde el backend");
@@ -6,15 +8,18 @@ export const test = (req, res) => {
 export const leerProductos = (req, res) => {};
 
 //agregar funcion para crear producto
-export const crearProducto = (req, res) => {
+export const crearProducto = async (req, res) => {
   try {
     //1- recibir el objeto que tengo que agregar a la bd
-    console.log(req.body);
     //2- validar los datos del objeto
     //3- guardar el objeto en la base de datos
+    const nuevoProducto = new Producto(req.body);
+    await nuevoProducto.save();
     //4- enviar respuesta
+    res.status(201).json({ mensaje: "Producto creado correctamente" });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ mensaje: "Error al crear el producto" });
   }
 };
 
